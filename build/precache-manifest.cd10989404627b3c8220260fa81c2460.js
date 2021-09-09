e548b46d6a5b4d5a3e0516a6a750137df9272b52
VihanR <71479823+VihanR@users.noreply.github.com>
2021-09-09 21:57:33 +1000

    first commit

---
+++ build/precache-manifest.cd10989404627b3c8220260fa81c2460.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf38bee48430b891f6e38ea37ab3e4fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d0434131db64a32e1ce",
+    "url": "/static/css/2.11822673.chunk.css"
+  },
+  {
+    "revision": "0fc35bd3da5aa0c31371",
+    "url": "/static/css/main.32404383.chunk.css"
+  },
+  {
+    "revision": "4d0434131db64a32e1ce",
+    "url": "/static/js/2.b3903a18.chunk.js"
+  },
+  {
+    "revision": "4effa9de81938b2545f40d4f7c3a8025",
+    "url": "/static/js/2.b3903a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc35bd3da5aa0c31371",
+    "url": "/static/js/main.b49cbda9.chunk.js"
+  },
+  {
+    "revision": "208783fa02780d630bac",
+    "url": "/static/js/runtime~main.402cc814.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
